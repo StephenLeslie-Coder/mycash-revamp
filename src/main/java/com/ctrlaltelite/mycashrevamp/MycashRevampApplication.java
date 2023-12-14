72db77f0fc8b5487376ec27c8191637891a5b49d
Shemar Brown-Wright <Shemar.Brown-Wright@digicelgroup.com>
2023-12-14 09:26:14 -0500

    refactored starter class

--- src/main/java/com/ctrlaltelite/mycashrevamp/MycashRevampApplication.java
+++ src/main/java/com/ctrlaltelite/mycashrevamp/MycashRevampApplication.java
@@ -1,18 +1,9 @@
 package com.ctrlaltelite.mycashrevamp;
 
-import com.ctrlaltelite.mycashrevamp.model.*;
-import com.ctrlaltelite.mycashrevamp.service.BlockchainService;
-import com.ctrlaltelite.mycashrevamp.service.WalletService;
-import com.ctrlaltelite.mycashrevamp.utils.CryptoUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication()
 public class MycashRevampApplication {
     public static void main(String[] args) {
         SpringApplication.run(MycashRevampApplication.class, args);
